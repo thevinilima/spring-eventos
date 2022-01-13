deb06f93932038064b6084760f61de0e103908dc
Vinicius Lima <thelimavinicius@gmail.com>
2022-01-13 17:02:19 -0300

    Aula 08

--- src/main/java/com/vinicius/SpringEventos/control/EventoController.java
+++ src/main/java/com/vinicius/SpringEventos/control/EventoController.java
@@ -4,6 +4,7 @@ import com.vinicius.SpringEventos.model.Evento;
 import com.vinicius.SpringEventos.repository.EventoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -32,6 +33,15 @@ public class EventoController {
         ModelAndView mv = new ModelAndView("index");
         Iterable<Evento> eventos = er.findAll();
         mv.addObject("eventos", eventos);
+
+        return mv;
+    }
+
+    @RequestMapping("/{codigo}")
+    public ModelAndView detalhesEvento(@PathVariable("codigo") long codigo) {
+        Evento evento = er.findByCodigo(codigo);
+        ModelAndView mv = new ModelAndView("evento/detalhesEvento");
+        mv.addObject("evento", evento);
 
         return mv;
     }
